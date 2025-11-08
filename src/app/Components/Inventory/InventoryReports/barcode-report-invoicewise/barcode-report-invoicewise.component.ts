07c2df2804413377024f424f5b66b270d716cc70
Najami007 <najamali244@gmail.com>
2025-11-09 00:20:22 +0500

    ...

--- src/app/Components/Inventory/InventoryReports/barcode-report-invoicewise/barcode-report-invoicewise.component.ts
+++ src/app/Components/Inventory/InventoryReports/barcode-report-invoicewise/barcode-report-invoicewise.component.ts
@@ -7,6 +7,7 @@ import { AppComponent } from 'src/app/app.component';
 import { GlobalDataModule } from 'src/app/Shared/global-data/global-data.module';
 import { NotificationService } from 'src/app/Shared/service/notification.service';
 import { environment } from 'src/environments/environment.development';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-barcode-report-invoicewise',
@@ -44,13 +45,24 @@ export class BarcodeReportInvoicewiseComponent implements OnInit {
 
   }
   ngOnInit(): void {
-    this.global.setHeaderTitle('Barcode Report Invoice')
+    this.global.setHeaderTitle('Barcode Report Invoice');
+
+
+    this.global.getProducts().subscribe(
+      (Response: any) => {
+        this.productList = Response;
+      }
+    )
   }
 
 
-
+  productList: any = [];
   showPrice: any = true;
   showTitle: any = true;
+  showBarcode:any = true;
+  showCompany:any = true;
+
+  PBarcode: any = '';
 
 
   SearchDate: any = new Date();
@@ -99,34 +111,41 @@ export class BarcodeReportInvoicewiseComponent implements OnInit {
         Response.forEach((e: any) => {
 
           this.myTotalQty += e.quantity;
-          this.tableDataList.push({
-            isChecked: false,
-            rowIndex: this.tableDataList.length + 1,
-            productID: e.productID,
-            productTitle: e.productTitle,
-            barcode: e.barcode,
-            productImage: e.productImage,
-            quantity: e.quantity,
-            wohCP: e.costPrice,
-            tempCostPrice: e.tempCostPrice,
-            margin: ((e.salePrice - e.costPrice) / e.costPrice) * 100,
-            costPrice: e.costPrice,
-            salePrice: e.salePrice,
-            expiryDate: this.global.dateFormater(new Date(e.expiryDate), '-'),
-            BatchNo: e.batchNo,
-            BatchStatus: e.batchStatus,
-            UomID: e.uomID,
-            Packing: e.packing,
-            discInP: e.discInP,
-            discInR: e.discInR,
-            AQ: e.aq,
-            gst: e.gst,
-            et: e.et,
-          })
+          this.pushProdData(e, false,'bill');
         });
       }
     )
 
+  }
+
+  pushProdData(item: any, isChecked: any,type:any) {
+
+    this.tableDataList.push({
+      isChecked: isChecked,
+      rowIndex: this.tableDataList.length + 1,
+      productID: item.productID,
+      productTitle: item.productTitle,
+      barcode: item.barcode,
+      productImage: item.productImage,
+      quantity: item.quantity,
+      wohCP: item.costPrice,
+      tempCostPrice: item.tempCostPrice,
+      margin: ((item.salePrice - item.costPrice) / item.costPrice) * 100,
+      costPrice: item.costPrice,
+      salePrice: item.salePrice,
+      // expiryDate: this.global.dateFormater(new Date(item.expiryDate), '-'),
+      BatchNo: item.batchNo,
+      BatchStatus: item.batchStatus,
+      UomID: item.uomID,
+      Packing: item.packing,
+      discInP: item.discInP,
+      discInR: item.discInR,
+      AQ: item.aq,
+      gst: item.gst,
+      et: item.et,
+      type:type
+
+    })
   }
 
 
@@ -211,7 +230,7 @@ export class BarcodeReportInvoicewiseComponent implements OnInit {
   getBarcodeWidth(value: string): number {
     // Base width (e.g., 2px per character, adjust as needed)
     const baseWidth = 1.5
-    if(value.length == 0) return 1;
+    if (value.length == 0) return 1;
 
     // Ensure a minimum width for short values
     // return Math.max(2, baseWidth); 
@@ -220,6 +239,191 @@ export class BarcodeReportInvoicewiseComponent implements OnInit {
     return Math.max(1, value.length < 10 ? 1.5 : 2 / value.length);
   }
 
+
+
+
+  rowFocused = -1;
+  prodFocusedRow = 0;
+  changeFocus(e: any, cls: any) {
+
+    if (e.target.value == '') {
+
+    } else {
+      this.prodFocusedRow = 0;
+      /////move down
+      if (e.keyCode == 40) {
+        if (this.productList.length >= 1) {
+          $('.prodRow0').trigger('focus');
+        }
+      }
+    }
+  }
+
+
+
+  searchByCode(e: any) {
+
+    if (this.PBarcode !== '') {
+      if (e.keyCode == 13) {
+        ///// check the product in product list by barcode
+        this.global.getProdDetail(0, this.PBarcode).subscribe(
+          (Response: any) => {
+            if (Response.length > 0) {
+              this.pushProdData(Response[0], true, 'extra')
+            } else {
+              this.msg.WarnNotify('Product Not Found!')
+            }
+
+          }
+        )
+
+        // var row =  this.productList.find((p:any)=> p.barcode == this.PBarcode);
+
+        // /////// check already present in the table or not
+        // if(row !== undefined){
+        //   this.curProduct = [];
+        //   this.curProduct.push(row);
+        // }else{
+        //   this.msg.WarnNotify('Product Not Found')
+        // }
+
+
+        this.PBarcode = '';
+        $('#searchProduct').trigger('focus');
+
+      }
+    }
+
+
+  }
+
+
+
+  onProductSelected(item: any) {
+    // this.curProduct.push(item);
+    this.global.getProdDetail(item.productID, '').subscribe(
+      (Response: any) => {
+        if (Response.length > 0) {
+          this.pushProdData(Response[0], true,'extra')
+        } else {
+          this.msg.WarnNotify('Product Not Found!')
+        }
+
+      }
+    )
+
+    this.PBarcode = '';
+    $('#searchProduct').trigger('focus');
+
+
+  }
+
+
+  handleProdFocus(item: any, e: any, cls: any, endFocus: any, prodList: []) {
+
+
+    /////// increment in prodfocus on tab click
+    if (e.keyCode == 9 && !e.shiftKey) {
+      this.prodFocusedRow += 1;
+
+    }
+    /////// decrement in prodfocus on shift tab click
+    if (e.shiftKey && e.keyCode == 9) {
+      this.prodFocusedRow -= 1;
+
+    }
+    /////move down
+    if (e.keyCode == 40) {
+
+
+      if (prodList.length > 1) {
+        this.prodFocusedRow += 1;
+        if (this.prodFocusedRow >= prodList.length) {
+          this.prodFocusedRow -= 1
+        } else {
+          var clsName = cls + this.prodFocusedRow;
+          //  alert(clsName);
+          $(clsName).trigger('focus');
+          e.which = 9;
+          $(clsName).trigger(e)
+        }
+      }
+    }
+
+
+    //Move up
+    if (e.keyCode == 38) {
+
+      if (this.prodFocusedRow == 0) {
+        $(endFocus).trigger('focus');
+        this.prodFocusedRow = 0;
+
+      }
+
+      if (prodList.length > 1) {
+
+        this.prodFocusedRow -= 1;
+
+        var clsName = cls + this.prodFocusedRow;
+        //  alert(clsName);
+        $(clsName).trigger('focus');
+
+
+      }
+
+    }
+
+  }
+
+
+  editQty(item: any) {
+
+    if(item.type == 'bill')return;
+
+        Swal.fire({
+          title: "Enter Total Amount",
+          input: "text",
+          showCancelButton: true,
+          confirmButtonText: 'Save',
+          showLoaderOnConfirm: true,
+          preConfirm: (value) => {
+  
+            if (!value || isNaN(value) || value <= 0) {
+              return Swal.showValidationMessage("Enter Valid Qty");
+            }
+            const index = this.tableDataList.indexOf(item);
+         
+  
+            this.tableDataList[index].quantity = value;
+          }
+        }).then((result) => {
+          if (result.isConfirmed) {
+            Swal.fire({
+              title: "Quantity Updated",
+              timer: 500,
+            });
+          }
+        })
+      }
+
+
+
+  tagList:any = [];
+
+  printTags(){
+
+    this.tagList = this.tableDataList.filter((e:any)=> e.isChecked == true);
+
+    console.log(this.tagList);
+
+    setTimeout(() => {
+          this.global.printData('#PrintTags');
+
+    }, 200);
+
+
+  }
+    
 
 
 }
